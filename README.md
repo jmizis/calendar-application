a397f8740bf0cc4e6ca0335ab548ba37d1e0eef8
jmizis <107073756+jmizis@users.noreply.github.com>
2022-08-23 15:56:54 -0400

    Final Push

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 Welcome to the calendar-application where you can fill out your daily work schdule for your busy day ahead!
 Before starting I created a new repository on GitHub and cloned it to my computer. 
 For this assignment we were given a tiny bit of starter code for the html and css.
+In html I used bootsrap to create my timeblocks for the calendar.
+Next I displayed bt time blocks with AM time starting at 9 going till 5PM
+Now that I have that completely I can work fully on the JavaScript part.
+In JavaScript I started with created my local storage.
+I create a function for when I press the save button everything in the text area will get saved.
+Give it a try! If you go to 10AM and write "meeting" press save and they refresh the page.
+Once it's refreshed it reappears!
+You can also go right click to Inspect, Application, Local storage file:// and see it also saved there as well. 
+Next came my timeUpdater function.
+Depending on what time of the day it is your schdule blocks will come up a specific color.
+Grey for past time, red for current, and green for the future.
+And last on my JavaScript you can find my displayTime function which shows you the current time in hh:mm:ss format at the head of your schdule. 
+Next using Gitbash I push my final work to my GitHub where I create my deploy site under settings.
+Once that is completed I go to my Bootcamp spot and submit my deplyed URL and my GitHub URL under Module 5 Challenge. 
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,47 +4,55 @@ var timeDisplayEl = document.querySelector("#currentDay")
 
 
 // 
-// }
+// This function creates my local storage for my text area
 $(".saveBtn").on("click",function(){
   var textValue = $(this).siblings(".description").val()
   var divId = $(this).parent().attr("id")
   window.localStorage.setItem(divId,textValue)
 })
-$("#9 .description").val(localStorage.getItem("9"))
 
+// Breaks down the hours 
+$("#9 .description").val(localStorage.getItem("9"))
+$("#10 .description").val(localStorage.getItem("10"))
+$("#11 .description").val(localStorage.getItem("11"))
+$("#12 .description").val(localStorage.getItem("12"))
+$("#13 .description").val(localStorage.getItem("13"))
+$("#14 .description").val(localStorage.getItem("14"))
+$("#15 .description").val(localStorage.getItem("15"))
+$("#16 .description").val(localStorage.getItem("16"))
+$("#17 .description").val(localStorage.getItem("17"))
+
+// This function creates my current time to average down and will allow my calendar to have diffrent color for past, present, and future.
 function timeUpdater () {
   var currentTime = moment().hours()
 
 
   $(".time-block").each(function(){
-  var divTime = $(this).attr("id")
-  if (divTime<currentTime){
-    $(this).addClass("past")
-  }
-  else if (divTime==currentTime){
-    $(this).addClass("present")
-  }
-  else if (divTime>currentTime){
-    $(this).addClass("future")
-  }
-  
+    var divTime = $(this).attr("id")
+    console.log(divTime)
+    
+    if (divTime<currentTime){
+      console.log($(this));
+      $(this)[0].classList.add(".past")
+      
+    } else if (divTime==currentTime){
+      //works
+      $(this)[0].classList.add(".present")
+    } else {
+      $(this)[0].classList.add(".future")
+    }
   })
 
+};
+ 
+timeUpdater();
+// Display the current time on top of my application
 
-}
-timeUpdater()
 function displayTime() {
     var rightNow = moment().format("MMM DD, YYYY  hh:mm:ss");
     timeDisplayEl.textContent = rightNow +"PM";
   }
   displayTime()
-   
+  
 
-// var studentGrade = {
-  //  student: student.value,
- //   grade: grade.value,
- //   comment: comment.value.trim()
- // };
-  // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
- // localStorage.setItem("studentGrade", JSON.stringify(studentGrade));
-//}
+//
